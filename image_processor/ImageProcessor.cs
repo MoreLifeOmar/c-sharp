6ace760d79e47d31d6ea13a08788d615859d8bdb
Omar Cherni <89669057+ocherni@users.noreply.github.com>
2023-02-02 22:35:20 +0100

    Update ImageProcessor.cs

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,147 +1,135 @@
 using System;
-using System.IO;
-using System.Threading.Tasks;
 using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
-/// <summary>ImageProcessor Class</summary>
+/// <summary>
+/// Represents an image processor.
+/// </summary>
 class ImageProcessor
 {
-    /// <summary>Method that inverts an image’s color</summary>
+    /// <summary>
+    /// Creates inverted images.
+    /// </summary>
+    /// <param name="filenames">The array of image filenames to invert.</param>
     public static void Inverse(string[] filenames)
     {
-        Parallel.ForEach(filenames, (currentFile) => {
-            string invertedImageName = Path.GetFileNameWithoutExtension(currentFile) + "_inverse" + Path.GetExtension(currentFile);
-            // Create a new bitmap.
-            Bitmap bmp = new Bitmap(currentFile);
+        ChangeFiles(filenames, "_inverse", (byte[] bytes) => {
 
-            // Lock the bitmap's bits.  
-            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
-            System.Drawing.Imaging.BitmapData bmpData =
-                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
-                bmp.PixelFormat);
-
-            // Get the address of the first line.
-            IntPtr ptr = bmpData.Scan0;
-
-            // Declare an array to hold the bytes of the bitmap.
-            int bytes  = Math.Abs(bmpData.Stride) * bmp.Height;
-            byte[] rgbValues = new byte[bytes];
-
-            // Copy the RGB values into the array.
-            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
-
-            // Invert every value 255 - byte value.  
-            for (int counter = 0; counter < rgbValues.Length; counter++)
-                rgbValues[counter] = (byte) (255 - rgbValues[counter]);
-
-            // Copy the RGB values back to the bitmap
-            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
-
-            // Unlock the bits.
-            bmp.UnlockBits(bmpData);
-
-            // Save the modified bitmap
-            bmp.Save(invertedImageName);
+            for (int i = 0; i < bytes.Length; i++)
+                bytes[i] = (byte)(255 - bytes[i]);
+                
         });
     }
 
-    /// <summary>Method that converts each image to grayscale</summary>
+    /// <summary>
+    /// Creates grayscale images.
+    /// </summary>
+    /// <param name="filenames">The array of image filenames to convert to grayscale.</param>
     public static void Grayscale(string[] filenames)
     {
-        Parallel.ForEach(filenames, (currentFile) => {
-            string grayScaleImageName = Path.GetFileNameWithoutExtension(currentFile) + "_grayscale" + Path.GetExtension(currentFile);
-            // Create a new bitmap.
-            Bitmap bmp = new Bitmap(currentFile);
+        ChangeFiles(filenames, "_grayscale", (byte[] bytes) => {
 
-            // Lock the bitmap's bits.  
-            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
-            System.Drawing.Imaging.BitmapData bmpData =
-                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
-                bmp.PixelFormat);
-
-            // Get the address of the first line.
-            IntPtr ptr = bmpData.Scan0;
-
-            // Declare an array to hold the bytes of the bitmap.
-            int bytes  = Math.Abs(bmpData.Stride) * bmp.Height;
-            byte[] rgbValues = new byte[bytes];
-
-            // Copy the RGB values into the array.
-            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
-
-            // Set RGB values to grayscale.  
-            for (int counter = 2; counter < rgbValues.Length; counter += 3)
+            for (int i = 0; i < bytes.Length - 2; i += 3)
             {
-                int grayscale = (int)(.11 * rgbValues[counter - 2] + .59 * rgbValues[counter - 1] + .3 * rgbValues[counter]);
-                rgbValues[counter - 2] = (byte) grayscale;
-                rgbValues[counter - 1] = (byte) grayscale;
-                rgbValues[counter] = (byte) grayscale;
-            }                
+                byte avg = (byte)((bytes[i] + bytes[i + 1] + bytes[i + 2]) / 3);
 
-            // Copy the RGB values back to the bitmap
-            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
-
-            // Unlock the bits.
-            bmp.UnlockBits(bmpData);
-
-            // Save the modified bitmap
-            bmp.Save(grayScaleImageName);
-        });
-    }
-
-    /// <summary>Method that reduces each image to only black and white values, based on a given threshold</summary>
-    public static void BlackWhite(string[] filenames, double threshold)
-    {
-        Parallel.ForEach(filenames, (currentFile) => {
-            string bwImageName = Path.GetFileNameWithoutExtension(currentFile) + "_bw" + Path.GetExtension(currentFile);
-            // Create a new bitmap.
-            Bitmap bmp = new Bitmap(currentFile);
-
-            // Lock the bitmap's bits.  
-            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
-            System.Drawing.Imaging.BitmapData bmpData =
-                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
-                bmp.PixelFormat);
-
-            // Get the address of the first line.
-            IntPtr ptr = bmpData.Scan0;
-
-            // Declare an array to hold the bytes of the bitmap.
-            int bytes  = Math.Abs(bmpData.Stride) * bmp.Height;
-            byte[] rgbValues = new byte[bytes];
-
-            // Copy the RGB values into the array.
-            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
-
-            // Set RGB values to 255 o 0 depending on the luminance.  
-            for (int counter = 2; counter < rgbValues.Length; counter += 3)
-            {
-                double luminance = .0722 * rgbValues[counter - 2] + .7152 * rgbValues[counter - 1] + .2126 * rgbValues[counter];
-                if (luminance >= threshold)
-                    rgbValues[counter - 2] = rgbValues[counter - 1] = rgbValues[counter] = 255;
-                else
-                    rgbValues[counter - 2] = rgbValues[counter - 1] = rgbValues[counter] = 0;
+                bytes[i] = avg;
+                bytes[i + 1] = avg;
+                bytes[i + 2] = avg;
             }
 
-            // Copy the RGB values back to the bitmap
-            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
-
-            // Unlock the bits.
-            bmp.UnlockBits(bmpData);
-
-            // Save the modified bitmap
-            bmp.Save(bwImageName);
         });
     }
 
-    /// <summary>Method that creates a thumbnail image based on a given height</summary>
+    /// <summary>
+    /// Creates black and white images.
+    /// </summary>
+    /// <param name="filenames">The array of image filenames to convert to black and white.</param>
+    /// <param name="threshold">The luminance threshold.</param>
+    public static void BlackWhite(string[] filenames, double threshold)
+    {
+        ChangeFiles(filenames, "_bw", (byte[] bytes) => {
+
+            for (int i = 0; i < bytes.Length - 2; i += 3)
+            {
+                byte val = 0;
+
+                if ((bytes[i] + bytes[i + 1] + bytes[i + 2]) >= threshold)
+                    val = 255;
+
+                bytes[i] = val;
+                bytes[i + 1] = val;
+                bytes[i + 2] = val;
+            }
+
+        });
+    }
+
+    /// <summary>
+    /// Creates thumbnail images.
+    /// </summary>
+    /// <param name="filenames">The array of image filenames to create thumbnails from.</param>
+    /// <param name="height">The height of the thumbnail (retains aspect ratio).</param>
     public static void Thumbnail(string[] filenames, int height)
     {
-        Parallel.ForEach(filenames, (currentFile) => {
-            string thumbnailImageName = Path.GetFileNameWithoutExtension(currentFile) + "_th" + Path.GetExtension(currentFile);
-            Bitmap imageBitmap = new Bitmap(currentFile);
-            Image thumbnail = imageBitmap.GetThumbnailImage(((imageBitmap.Width * height) / imageBitmap.Height), height, () => false, IntPtr.Zero);
-            thumbnail.Save(thumbnailImageName);
+        Parallel.ForEach(filenames, (filename) => {
+            string name = Path.GetFileNameWithoutExtension(filename);
+            string extension = Path.GetExtension(filename);
+
+            Image im = Image.FromFile(filename);
+            int width = im.Width / (im.Height / height);
+            Image thumb = im.GetThumbnailImage(width, height, ()=>false, IntPtr.Zero);
+
+            thumb.Save($"{name}_th{extension}");
         });
+    }
+
+    /// <summary>
+    /// Processes files in parallel.
+    /// </summary>
+    /// <param name="filenames">The array of filenames to process.</param>
+    /// <param name="app">The filename suffix before extension.</param>
+    /// <param name="f">The processing function.</param>
+    public static void ChangeFiles(string[] filenames, string app, Action<byte[]> f)
+    {
+        Parallel.ForEach(filenames, (filename) => {
+            BitmapHelper(f, filename, app);
+        });
+    }
+
+    /// <summary>
+    /// Creates locked bitmaps for images.
+    /// </summary>
+    /// <param name="f">The processing function.</param>
+    /// <param name="filename">The original filename.</param>
+    /// <param name="app">The filename suffix before extension.</param>
+    public static void BitmapHelper(Action<byte[]> f, string filename, string app)
+    {
+        string name = Path.GetFileNameWithoutExtension(filename);
+        string extension = Path.GetExtension(filename);
+
+        Bitmap bm = new Bitmap(filename);
+
+        Rectangle rect = new Rectangle(0, 0, bm.Width, bm.Height);
+        System.Drawing.Imaging.BitmapData bmData =
+            bm.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+            bm.PixelFormat);
+
+        IntPtr p = bmData.Scan0;
+
+        int bytes = Math.Abs(bmData.Stride) * bm.Height;
+        byte[] rgbValues = new byte[bytes];
+
+        System.Runtime.InteropServices.Marshal.Copy(p, rgbValues, 0, bytes);
+
+        f(rgbValues);
+
+        System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, p, bytes);
+
+        bm.UnlockBits(bmData);
+        
+        bm.Save($"{name}{app}{extension}");
     }
 }
